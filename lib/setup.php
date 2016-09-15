62bf407ce6235271a60ccab3e0744105e86b8e61
Samuel Lie <samuel.lie1@gmail.com>
2016-09-15 11:34:31 +0200

    Only run video script on contact template

--- lib/setup.php
+++ lib/setup.php
@@ -122,6 +122,8 @@ function assets() {
 
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
   wp_enqueue_script('navigation', Assets\asset_path('scripts/navigation.js'), ['jquery'], null, true);
-  wp_enqueue_script('video-section', Assets\asset_path('scripts/video-section.js'), ['jquery'], null, true);
+  if ( is_page_template('template-contact.php') ) {
+    wp_enqueue_script('video-section', Assets\asset_path('scripts/video-section.js'), ['jquery'], null, true);
+  }
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
