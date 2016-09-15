b7bf8ace5c8b70e1932bea56de44e07bd36ce768
Samuel Lie <samuel.lie1@gmail.com>
2016-09-15 11:48:18 +0200

    Play video when at least quarter of element is in view

--- assets/scripts/video-section.js
+++ assets/scripts/video-section.js
@@ -6,21 +6,25 @@
     videoPlayed = true;
   });
 
+  // Calculate if the element is scrolled in view so that at least a quarter of the video is in view
   function scrolledIntoView(el) {
     var docViewTop    = $(window).scrollTop(),
         docViewBottom = docViewTop + $(window).height(),
         elTop         = $(el).offset().top,
-        elBottom      = elTop + $(el).height();
+        elHeight      = $(el).height(),
+        elBottom      = elTop + elHeight;
 
-    return ( (elBottom <= docViewBottom) && (elTop >= docViewTop) );
+    return ( (elTop + elHeight*0.25 <= docViewBottom) && (elBottom - elHeight*0.25 >= docViewTop) );
   }
 
   $(window).scroll(function() {
     if ( scrolledIntoView('.video-section') && videoPlayed === false ) {
       video.get(0).play();
+      console.log('play');
     }
     else if (videoPlayed === false) {
       video.get(0).pause();
+      console.log('pause');
     }
   });
 })(jQuery);
